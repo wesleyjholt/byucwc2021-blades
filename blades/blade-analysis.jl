c62ab312caecb1d783df2576ff53c9407bb5ca3a
Wesley Holt <wesleyjholt@gmail.com>
2021-02-01 09:54:13 -0700

    set up and debug analysis

--- blades/blade-analysis.jl
+++ blades/blade-analysis.jl
@@ -3,6 +3,7 @@
 # import packages
 using CCBlade
 using PyPlot
+using DelimitedFiles
 
 # specify which blade to analyze
 blade = "pennstate2019"
@@ -25,7 +26,7 @@ tilt = 0.0*pi/180
 
 # create vector of tip-speed ratios to analyze
 ntsr = 20  # number of tip-speed ratios
-tsrvec = range(1, 8, length=ntsr)
+tsrvec = range(.5, 5, length=ntsr)
 
 # initialize arrays to hold values for power, thrust, and torque coefficients
 cpvec = zeros(ntsr)
@@ -35,10 +36,12 @@ cqvec = zeros(ntsr)
 # create vector of blade azimuth angles at which to analyze turbine performance
 azangles = pi/180*[0.0, 90.0, 180.0, 270.0]
 
+# function solve_
+
 # perform analysis for each tip-speed ratio
 for i = 1:ntsr
-    local Omega = Vinf*tsrvec[i]/rotorR
 
+    local Omega = Vinf*tsrvec[i]/rotorR
     local ops = windturbine_op.(Vinf, Omega, pitch, r, precone, yaw, tilt, azangles', hubHt, shearExp, rho)
     local outs = solve.(Ref(rotor), sections, ops)
     local T, Q = thrusttorque(rotor, sections, outs)
@@ -46,14 +49,23 @@ for i = 1:ntsr
     cpvec[i], ctvec[i], cqvec[i] = nondim(T, Q, Vinf, Omega, rho, rotor, "windturbine")
 end
 
+# get QBlade data
+qblade_data = readdlm("input-files/pennstate2019-nondimpowercurve-qblade.txt", skipstart=3)
+qblade_tsrvec = qblade_data[:,12*2-1]
+qblade_cpvec = qblade_data[:,12*2]
+
 # plot nondimensional power curve (power coefficient vs. tip-speed ratio)
 figure()
 plot(tsrvec, cpvec)
-plot(tsrvec, ctvec)
+# plot(tsrvec, ctvec)
+plot(qblade_tsrvec, qblade_cpvec)
 xlabel("tip speed ratio")
-legend([L"C_P", L"C_T"])
+legend([L"C_P"*" - CCBlade", L"C_T"*" - QBlade"])
 savefig(string("figures/", blade, "-nondimpowercurve.pdf"))
 
+maxcp, maxcp_ind = findmax(cpvec)
+println("optimal tip-speed ratio is: ", tsrvec[maxcp_ind])
+println("max cp: ", maxcp)
 
 
 # Vinf = 10.0
--- blades/params/pennstate2019.jl
+++ blades/params/pennstate2019.jl
@@ -3,6 +3,8 @@
 # import packages
 using CCBlade
 using DelimitedFiles
+using PyPlot
+import FLOWMath
 
 # import the radial section properties
 sectiondata = readdlm("input-files/pennstate2019.txt", skipstart=1)
@@ -11,11 +13,28 @@ sectiondata = readdlm("input-files/pennstate2019.txt", skipstart=1)
 r = sectiondata[:,1]
 chord = sectiondata[:,2]
 theta = sectiondata[:,3]*pi/180
+# figure()
+# plot(chord, r)
+# xlabel("radial position")
+# ylabel("chord")
+# plt.show()
+
+# import Wortmann FX 63-137 airfoil data
+# Re_vec = [0.005, 0.006, 0.007, 0.008, 0.009, 0.01, 0.015, 0.02, 0.025, 0.03, 0.035, 0.04, 0.045, 
+#     0.05, 0.055, 0.06, 0.065, 0.07, 0.075, 0.08, 0.085, 0.09, 0.095, 0.1, 0.13, 0.16, 0.2, 0.25, 0.3] .* 1e6
+Re_vec = [0.005, 0.008, 0.01, 0.015, 0.02, 0.025, 0.03, 0.035, 0.04, 0.045, 0.05, 0.055, 0.06, 0.065, 0.07, 0.075, 0.08, 0.085, 0.09, 0.095, 0.1, 0.13, 0.16, 0.2, 0.25, 0.3] .* 1e6
+airfoildata_directory = "airfoil-data/wortmann-fx63137/"
+wortmann_fx_63137_filenames = Vector{String}(undef, length(Re_vec))
+for i = 1:length(Re_vec)
+    wortmann_fx_63137_filenames[i] = airfoildata_directory * "wortmann-fx63137-extrapolated-Re" * rpad(Re_vec[i]/1e6, 5, '0') * "e6.dat"
+    alpha_vec = readdlm(wortmann_fx_63137_filenames[i], skipstart=3)[:,1]
+end
+wortmann_fx_63137 = AlphaReAF(wortmann_fx_63137_filenames; radians=true)
 
 # Define airfoil(s). This blade only has one airfoil that is used throughout the entire length.
-naf = 1
-aftypes = Array{AlphaAF}(undef, naf)
-aftypes[1] = AlphaAF("airfoil-data/NACA64_A17.dat", radians=false)
+naf = length(r)
+aftypes = Array{AlphaReAF}(undef, naf)
+aftypes[1] = wortmann_fx_63137
 
 # indices correspond to which airfoil is used at which station
 af_idx = Int.(sectiondata[:,4])
